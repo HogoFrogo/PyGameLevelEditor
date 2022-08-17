e8b79fe99bcdb156916ee1bf71af990363d81c15
Samuel Machat <35379505+SamuelMachat@users.noreply.github.com>
2022-08-17 17:45:52 +0200

    Changed LevelEditor to be able to edit terrain files

--- level_editor_tut.py
+++ level_editor_tut.py
@@ -19,33 +19,34 @@ pygame.display.set_caption('Level Editor')
 
 
 #define game variables
-ROWS = 16
+ROWS = 11
 MAX_COLS = 150
 TILE_SIZE = SCREEN_HEIGHT // ROWS
-TILE_TYPES = 21
+TILE_TYPES = 16
 level = 0
 current_tile = 0
 scroll_left = False
 scroll_right = False
 scroll = 0
 scroll_speed = 1
-
+tile_heights = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+# tile_heights = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2]
 
 #load images
-pine1_img = pygame.image.load('img/Background/pine1.png').convert_alpha()
-pine2_img = pygame.image.load('img/Background/pine2.png').convert_alpha()
-mountain_img = pygame.image.load('img/Background/mountain.png').convert_alpha()
-sky_img = pygame.image.load('img/Background/sky_cloud.png').convert_alpha()
+bg_img_2 = pygame.image.load('img/Background/bg_img_2.png').convert_alpha()
+bg_img_3 = pygame.image.load('img/Background/bg_img_3.png').convert_alpha()
+bg_img_1 = pygame.image.load('img/Background/bg_img_1.png').convert_alpha()
+sky_img = pygame.image.load('img/Background/sky.png').convert_alpha()
 #store tiles in a list
 img_list = []
 for x in range(TILE_TYPES):
-	img = pygame.image.load(f'img/tile/{x}.png').convert_alpha()
-	img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
+	img = pygame.image.load(f'img/tile2/{x}.png').convert_alpha()
+	img_height = tile_heights[x]
+	img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE*img_height))
 	img_list.append(img)
 
 save_img = pygame.image.load('img/save_btn.png').convert_alpha()
 load_img = pygame.image.load('img/load_btn.png').convert_alpha()
-
 
 #define colours
 GREEN = (144, 201, 120)
@@ -78,9 +79,9 @@ def draw_bg():
 	width = sky_img.get_width()
 	for x in range(4):
 		screen.blit(sky_img, ((x * width) - scroll * 0.5, 0))
-		screen.blit(mountain_img, ((x * width) - scroll * 0.6, SCREEN_HEIGHT - mountain_img.get_height() - 300))
-		screen.blit(pine1_img, ((x * width) - scroll * 0.7, SCREEN_HEIGHT - pine1_img.get_height() - 150))
-		screen.blit(pine2_img, ((x * width) - scroll * 0.8, SCREEN_HEIGHT - pine2_img.get_height()))
+		screen.blit(bg_img_1, ((x * width) - scroll * 0.6, SCREEN_HEIGHT - bg_img_1.get_height() - 300))
+		screen.blit(bg_img_2, ((x * width) - scroll * 0.7, SCREEN_HEIGHT - bg_img_2.get_height() - 150))
+		screen.blit(bg_img_3, ((x * width) - scroll * 0.8, SCREEN_HEIGHT - bg_img_3.get_height()))
 
 #draw grid
 def draw_grid():
@@ -97,7 +98,8 @@ def draw_world():
 	for y, row in enumerate(world_data):
 		for x, tile in enumerate(row):
 			if tile >= 0:
-				screen.blit(img_list[tile], (x * TILE_SIZE - scroll, y * TILE_SIZE))
+				img_height = tile_heights[tile]
+				screen.blit(img_list[tile], (x * TILE_SIZE - scroll, (y-(img_height-1)) * TILE_SIZE))
 
 
 
@@ -126,13 +128,13 @@ while run:
 	draw_grid()
 	draw_world()
 
-	draw_text(f'Level: {level}', font, WHITE, 10, SCREEN_HEIGHT + LOWER_MARGIN - 90)
-	draw_text('Press UP or DOWN to change level', font, WHITE, 10, SCREEN_HEIGHT + LOWER_MARGIN - 60)
+	draw_text('UP/DOWN - change level, LEFT/RIGHT - scroll, +RShift - speed', font, WHITE, 10, SCREEN_HEIGHT + LOWER_MARGIN - 90)
+	draw_text(f'Level: {level}', font, WHITE, 10, SCREEN_HEIGHT + LOWER_MARGIN - 60)
 
 	#save and load data
 	if save_button.draw(screen):
 		#save level data
-		with open(f'level{level}_data.csv', 'w', newline='') as csvfile:
+		with open(f'level_{level}_terrain.csv', 'w', newline='') as csvfile:
 			writer = csv.writer(csvfile, delimiter = ',')
 			for row in world_data:
 				writer.writerow(row)
@@ -144,7 +146,7 @@ while run:
 		#load in level data
 		#reset scroll back to the start of the level
 		scroll = 0
-		with open(f'level{level}_data.csv', newline='') as csvfile:
+		with open(f'level_{level}_terrain.csv', newline='') as csvfile:
 			reader = csv.reader(csvfile, delimiter = ',')
 			for x, row in enumerate(reader):
 				for y, tile in enumerate(row):
